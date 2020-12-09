55f78c67f9b852f3c2d7f4b0148897c21606fc40
gra20h1 Aron Szakacs (aszakacs) Toni Tanner (tannerto) <gra20h1@badile24.ee.ethz.ch>
2020-12-09 14:33:48 +0100

    added paper to xilinx peripherals

--- fpga/src/ariane_peripherals_xilinx.sv
+++ fpga/src/ariane_peripherals_xilinx.sv
@@ -19,7 +19,8 @@ module ariane_peripherals #(
     parameter bit InclSPI      = 0,
     parameter bit InclEthernet = 0,
     parameter bit InclGPIO     = 0,
-    parameter bit InclTimer    = 1
+    parameter bit InclTimer    = 1,
+    parameter bit InclPAPER    = 1
 ) (
     input  logic       clk_i           , // Clock
     input  logic       clk_200MHz_i    ,
@@ -30,6 +31,8 @@ module ariane_peripherals #(
     AXI_BUS.Slave      gpio            ,
     AXI_BUS.Slave      ethernet        ,
     AXI_BUS.Slave      timer           ,
+    AXI_BUS.Master     paper_ms        ,
+    AXI_BUS.Slave      paper_sl        ,
     output logic [1:0] irq_o           ,
     // UART
     input  logic       rx_i            ,
@@ -826,6 +829,60 @@ module ariane_peripherals #(
             .PREADY  ( timer_pready     ),
             .PSLVERR ( timer_pslverr    ),
             .irq_o   ( irq_sources[6:3] )
+        );
+    end
+    if (InclPAPER) begin : gen_paper
+        localparam int                            SC_DEPTH = 128;
+        localparam int                            FILL_THRESH = 64;
+        localparam int                            DC_DEPTH = 24;
+        localparam logic [AxiIdWidth-1:0]         ARID = 'd213; // random constant value because PAPER doesn't do out of order txs
+
+        AXI_LITE #(
+            .AXI_ADDR_WIDTH(AxiAddrWidth),
+            .AXI_DATA_WIDTH(AxiDataWidth)
+        ) paper_lite_sl ();
+
+        axi_to_axi_lite
+        #(
+            .NUM_PENDING_RD   ( 10   ),
+            .NUM_PENDING_WR   ( 10   )
+        )
+        i_axi_to_axi_lite_paper_sl
+        (
+            .clk_i                 ( clk_i              ),
+            .rst_ni                ( rst_ni             ),
+            .testmode_i            ( 1'b0               ),
+            .in                    ( paper_sl           ),
+            .out                   ( paper_lite_sl      )
+        );
+
+        AXI2HDMI
+        #(
+            .AXI4_ADDRESS_WIDTH(AxiAddrWidth),
+            .AXI4_DATA_WIDTH(AxiDataWidth),
+            .AXI4_LITE_DATA_WIDTH(AxiDataWidth),
+            .AXI4_ID_WIDTH(AxiIdWidth),
+            .SC_FIFO_DEPTH(SC_DEPTH),
+            .FILL_THRESH(FILL_THRESH),
+            .DC_FIFO_DEPTH(DC_DEPTH),
+            .AXI_ARID(ARID),
+            .XILINX(1'b0),
+            .FONT_MEMINIT_FILE(0)
+        )
+        i_paper
+        (
+            .AXI_ACLK_CI(clk_i),
+            .AXI_ARESETn_RBI(rst_ni),
+            .AXIMaster(paper_ms), // r_data is input -> here paper is the master
+            .LiteSlave(paper_lite_sl.Slave), // lite <> normal issue still to be fixed
+            .PixelClk_CI(PxClk_C),
+            .PxClkRst_RBI(MMCMLocked_S),
+            .DOut_DO(Data422SDR_D),
+            .DE_SO(DE_S),
+            .HSync_SO(HSync_S),
+            .VSync_SO(VSync_S),
+            .SCEmpty_SO(SCEmpty_SO),
+            .DCEmpty_SO(DCEmpty_SO)
         );
     end
 endmodule
